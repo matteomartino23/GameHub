e2dd260ee8b1f24d87ef844f66fb62627311904e
matteomartino23 <136102412+matteomartino23@users.noreply.github.com>
2026-06-21 15:52:38 +0200

    Games.lua

--- Games.lua
+++ Games.lua
@@ -1,8 +1,6 @@
-local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/matteomartino23/GameHub/refs/heads/main/UI.lua"))()
-local Games = {}
-
-function Games:AddGame(tab, name id)
-  tab:CreateGameTeleport(name, id)
-end
-
-return Games
+return {
+  ["None"] = {
+    Name = "None",
+    Id = 0
+  }
+}
